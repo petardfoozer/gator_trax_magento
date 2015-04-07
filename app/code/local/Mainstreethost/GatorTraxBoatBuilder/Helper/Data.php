7c6e1383fccf5060196bf272de625f30ae703247
Brian Walleshauser <bwalleshauser@mainstreethost>
2015-04-07 12:12:06 -0400

    added gator module

---
+++ app/code/local/Mainstreethost/GatorTraxBoatBuilder/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bwalleshauser
+ * Date: 4/7/2015
+ * Time: 12:10 PM
+ */ 
+class Mainstreethost_GatorTraxBoatBuilder_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
