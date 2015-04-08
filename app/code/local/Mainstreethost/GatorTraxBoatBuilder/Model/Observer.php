b42361bc97f0275369e96cd4154ce361a634ecd6
Peter Cageao <pcageao@mainstreethost.com>
2015-04-08 15:24:45 -0400

    On select clears the cart and adds a selected boat to the cart

--- app/code/local/Mainstreethost/GatorTraxBoatBuilder/Model/Observer.php
+++ app/code/local/Mainstreethost/GatorTraxBoatBuilder/Model/Observer.php
@@ -2,9 +2,23 @@
 
 class Mainstreethost_GatorTraxBoatBuilder_Model_Observer
 {
+
+    public function __construct(Varien_Event_Observer $observer)
+    {
+
+        $checkCMSPage = Mage::app()->getFrontController()->getAction()->getFullActionName();
+        if ($checkCMSPage === 'cms_page_view') {
+            if ($this->isCustomerLoggedIn()) {
+                $this->getCustomerCart();
+                $this->getGatorProducts();
+                $this->addProductToCart();
+            }
+        }
+    }
+
     public function getGatorProducts()
     {
-           $cmsIdentifier = Mage::getStoreConfig('boatbuildersettings/boatbuildersettingsgroup/boatbuildercmspage');
+        $cmsIdentifier = Mage::getStoreConfig('boatbuildersettings/boatbuildersettingsgroup/boatbuildercmspage');
            if($cmsIdentifier == 'boat-builder') {
                $_productCollection = Mage::getModel('catalog/product')
                    ->getCollection()
@@ -16,5 +30,48 @@ class Mainstreethost_GatorTraxBoatBuilder_Model_Observer
                }
                return $_gatorBoatBuilderProducts;
            }
+    }
+
+    public function isCustomerLoggedIn()
+    {
+        if(Mage::getSingleton('customer/session')->isLoggedIn()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public function getCustomerCart()
+    {
+
+        $session = Mage::getSingleton('checkout/session');
+        $session->getQuote()->delete();
+        $session->clear();
+
+        $cart = Mage::getModel('checkout/cart');
+        $cart->setQuote($session->getQuote());
+
+        $this->clearCustomerCart();
+
+    }
+
+    public function clearCustomerCart()
+    {
+        Mage::helper('gator/Cart')->ClearCart();
+    }
+
+    public function addProductToCart()
+    {
+        $product_model = Mage::getSingleton('catalog/product');
+        $sku = "BOAT-MODEL-GEN-II";
+        $product_id = $product_model->getIdBySku($sku);
+        $product = $product_model->load($product_id);
+        $qty = 1;
+
+        $cart = Mage::getModel('checkout/cart');
+        $cart->addProduct($product, array('qty' => $qty));
+        $cart->save();
+        Mage::getSingleton('checkout/session')->setCartWasUpdated(true);
     }
 }
